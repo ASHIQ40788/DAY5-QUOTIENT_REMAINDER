ba68748b7f269c8449cf43893d5bb4bc7e96ebdf
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-07 18:53:57 +0530

    [ASHIQ] added : QUOTIENT_REMAINDER

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUOTIENT_REMAINDER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int dividend = 150040788, divisor = 5;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
